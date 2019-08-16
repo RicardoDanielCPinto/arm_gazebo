f4107af10f68ab8765804aa09d83d6f65a1ff7eb
RIcardo Pinto <ricardo.daniel.pinto97@gmail.com>
2019-08-16 17:21:53 +0100

    Remove duplicated directory

--- arm_gazebo/models/robotic_arm/refresh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Ricardo Pinto
-#Este programa serve para garantir que os ficheiros URDF e SDF são gerados e atualizados
-#através dos seus ficheiros PAI .xacro e .erb respetivamente
-
-erb model.erb > model.sdf
-xacro model.urdf.xacro > model.urdf
